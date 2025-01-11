b72f878369410206f29a7f1b8ee6c49c5e689730
Dilip Soni <dilipsoni37@gmail.com>
2025-01-12 00:05:41 +0530

    added code

--- src/views/customers/index.vue
+++ src/views/customers/index.vue
@@ -114,14 +114,6 @@
                       History
                     </span></b-dropdown-item
                   >
-                  <b-dropdown-item
-                    :href="'#/admin-booking-history/' + props.row.ids"
-                    ><span class="text-primary">
-                      <i class="mdi mdi-account-card-details"></i> Admin Booking
-                      History
-                    </span></b-dropdown-item
-                  >
-
                   <b-dropdown-item :href="'#/customer/' + props.row.ids"
                     ><span class="text-primary">
                       <i class="mdi mdi-pencil"></i> Edit
